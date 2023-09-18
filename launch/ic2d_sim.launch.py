4cc4829265b9a303424ed61cca09a2885a1a7e92
Leonardo F. dos Santos <leonardo.felipe.santos@usp.br>
2023-09-18 14:41:50 -0300

    Added gazebo_ros2_control interface and rqt_joint_trajectory_controller

--- launch/ic2d_sim.launch.py
+++ launch/ic2d_sim.launch.py
@@ -2,7 +2,12 @@ import os
 import xacro
 from launch_ros.actions import Node
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import (
+    IncludeLaunchDescription, 
+    ExecuteProcess, 
+    RegisterEventHandler
+)
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
@@ -19,7 +24,8 @@ def generate_launch_description():
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        output='screen',
+        name='robot_state_publisher',
+        output='both',
         parameters=[{'robot_description': robot_description_raw,
         'use_sim_time': True}]
     )
@@ -39,11 +45,54 @@ def generate_launch_description():
                    '-entity', 'ic2d']
     )
 
+    rqt_joint_controller = Node(
+        package='rqt_joint_trajectory_controller',
+        executable='rqt_joint_trajectory_controller',
+        output='screen',
+    )
+
+    joint_state_controller = ExecuteProcess(
+        cmd=[
+            'ros2',
+            'control',
+            'load_controller',
+            '--set-state',
+            'active',
+            'joint_state_broadcaster',
+        ],
+        output='both',
+    )
+
+    joint_trajectory_controller = ExecuteProcess(
+        cmd=[
+            'ros2',
+            'control',
+            'load_controller',
+            '--set-state',
+            'active',
+            'joint_trajectory_controller',
+        ],
+        output='both',
+    )
+
     # Run nodes
     return LaunchDescription([
         gazebo,
+        RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=spawn_entity,
+                    on_exit=[joint_state_controller],
+                )
+            ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_controller,
+                on_exit=[joint_trajectory_controller],
+            )
+        ),
         robot_state_publisher,
-        spawn_entity
+        rqt_joint_controller,
+        spawn_entity,
     ])
 
 
